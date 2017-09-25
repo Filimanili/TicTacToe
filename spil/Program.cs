756afb853e2c4abb781a3a2adbeac705e5d348db
Filip Malinowski <fili1028@edu.eal.dk>
2017-09-25 14:31:51 +0200

    HALLØJ tilføjet

--- spil/Program.cs
+++ spil/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace spil
 { //Hej med jer
   //Virker det?
+  //HALLØJ
     class Program
     {
         static void Main(string[] args) // Kører selve programmet
